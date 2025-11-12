12bb55b96c4ae3ea979ec9eb1fe738455b31317b
Producer234 <prexplanation2@gmail.com>
2025-11-12 19:42:48 +0600

    Update config.py

--- config.py
+++ config.py
@@ -13,10 +13,10 @@ SHORT_API = ""
 SHORT_TUT = "https://t.me/How_to_Download_7x/26"
 
 # Bot Configuration
-SESSION = "yato"
+SESSION = "PR"
 TOKEN = "642712"
-API_ID = ""
-API_HASH = ""
+API_ID = "26106561"
+API_HASH = "6bf2b0a825260beafe9955ffed29f978"
 WORKERS = 5
 
 DB_URI = "mongodb"
